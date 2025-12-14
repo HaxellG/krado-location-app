ba4156b221066193cdb9afb12ddd60e5c0bbc890
Haxell Gómez Lara <118084031+HaxellG@users.noreply.github.com>
2025-12-13 19:49:23 -0500

    Update comments for clarity in AWSBootstrap.swift

--- location/AWSBootstrap.swift
+++ location/AWSBootstrap.swift
@@ -5,21 +5,22 @@ import AWSCognitoIdentityProvider
 
 enum AWSBootstrap {
     static func configure() {
+        // 1) Configuración del Pool
         let identityPoolId = "POOL_IDENTIFIER"
         let region: AWSRegionType = .USEast1
         
-        // 1) Credenciales de Cognito
+        // 2) Credenciales de Cognito
         let credentialsProvider = AWSCognitoCredentialsProvider(
             regionType: region,
             identityPoolId: identityPoolId)
         
-        // 2) Configuración base
+        // 3) Configuración base
         let config = AWSServiceConfiguration(
             region: region,
             credentialsProvider: credentialsProvider)!
         AWSServiceManager.default().defaultServiceConfiguration = config
         
-        // 3) Registrar utilidades con claves
+        // 4) Registrar utilidades con claves
         AWSS3TransferUtility.register(with: config, forKey: "S3")
         AWSDynamoDB.register(with: config, forKey: "DDB")
         
